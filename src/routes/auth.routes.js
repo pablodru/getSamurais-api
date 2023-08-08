8af74b6385e0796a9bff9d08bd117e35ba1c7fa6
pablodru <pdrocha03@gmail.com>
2023-08-08 20:17:22 -0300

    initializing repository - base of project

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,7 @@
+import { Router } from "express";
+
+const authRouter = Router();
+
+//ROTASSSSSSSSSSSSSS
+
+export default authRouter;
---
+++ src/routes/index.routes.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import authRouter from "./auth.routes.js";
+import servicesRouter from "./services.routes.js";
+import usersRouter from "./users.routes.js";
+
+const router = Router();
+
+router.use(authRouter);
+router.use(servicesRouter);
+router.use(usersRouter);
