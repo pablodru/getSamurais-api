79ee937446f4d02a1d30dcc86e76aa08f56d14f9
pablodru <pdrocha03@gmail.com>
2023-08-14 12:41:17 -0300

    fix: changing sql query to order by asc

--- src/repository/users.repository.js
+++ src/repository/users.repository.js
@@ -3,7 +3,7 @@ import { db } from "../database/database.connection.js";
 export async function getMyServicesDB (userId){
 
     const query = `
-        SELECT services.status AS status, services.photo AS photo, services.name AS service, services.price AS price, services.id AS id FROM services
+        SELECT users.name AS name, services.status AS status, services.photo AS photo, services.name AS service, services.price AS price, services.id AS id FROM services
             JOIN users ON users.id = services."userId"
             WHERE services."userId"=$1
             ORDER BY services.id ASC
